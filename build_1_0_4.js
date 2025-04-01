da1533a7df4f295da238522239fd0a8497bc6b0b
GnOM <gnom.alexx@gmail.com>
2025-04-01 12:50:36 +0300

    1.0.4.6b16

--- build_1_0_4.js
+++ build_1_0_4.js
@@ -6,11 +6,11 @@ function unityShowBanner(msg, type) {
 }
 
 let buildUrl = "https://alexx-gnom.github.io/PoliticalIdle/Build";
-let loaderUrl = buildUrl + "/f01ec2f5314ec2c4f8b617c54efa2dd8.loader.js";
+let loaderUrl = buildUrl + "/e90ec41d16dc097de32c3b06e6a76e09.loader.js";
 let config = {
-    dataUrl: buildUrl + "/67d0a142e5e7782728296b71aee63227.data",
-    frameworkUrl: buildUrl + "/1753b36ece68dcaae83538b4e427ea07.framework.js",
-    codeUrl: buildUrl + "/c60271fc44d85cdad830db7084098ceb.wasm",
+    dataUrl: buildUrl + "/afe52b1e00baf4ef9b1a4d9f671fde63.data",
+    frameworkUrl: buildUrl + "/573e2f8e9eb4a46af02c5a36937c6dbb.framework.js",
+    codeUrl: buildUrl + "/8347f22009b84a3b8cea55fedc6a9dce.wasm",
     streamingAssetsUrl: "https://alexx-gnom.github.io/PoliticalIdle/StreamingAssets",
     companyName: "Trump’s Empire",
     productName: "Trump’s Empire",
